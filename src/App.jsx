638de71b275510846c79895742b6708509c165e6
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-16 10:02:49 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BookList } from "./components/books/BookList";
+import { Home } from "./components/home/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/booklist" element={<BookList />} />
     </Routes>
   );
