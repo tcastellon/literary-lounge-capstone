d29c11b012a3498d52b7784cd1cfdb2685301c20
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-16 15:09:30 -0500

    Updated book service to also get books by id

--- src/service/bookService.js
+++ src/service/bookService.js
@@ -1,3 +1,7 @@
 export const getAllBooks = () => {
     return fetch(`http://localhost:8088/books`).then(res => res.json())
 }
+
+export const getBooksById = (bookId) => {
+    return fetch(`http://localhost:8088/books/${bookId}`).then(res => res.json())
+}
