43d31361060ba01874ace5531acd964fae3ae539
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-17 15:09:43 -0500

    Updated a fetch function

--- src/service/readerService.js
+++ src/service/readerService.js
@@ -6,6 +6,6 @@ export const getReaderById = (readerId) => {
     return fetch(`http://localhost:8088/readers/${readerId}?_embed=booksRead`).then(res => res.json())
 }
 
-export const getReadBooks = (readerId) => {
+export const getBooksByReaderId = (readerId) => {
     return fetch(`http://localhost:8088/booksRead?readerId=${readerId}&_expand=book`).then(res => res.json())
 }
