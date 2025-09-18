dcb7174a804e05e98207e14462e737ca024c049c
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-18 11:14:23 -0500

    Added logout functionality

--- src/components/nav/ReaderNav.jsx
+++ src/components/nav/ReaderNav.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
 export const ReaderNav = () => {
+    const navigate = useNavigate()
     
     return (
         <ul>
@@ -12,6 +15,22 @@ export const ReaderNav = () => {
             <li>
                 <Link to="/readerlist">Readers</Link>
             </li>
+            {localStorage.getItem("lounge_user") ? (
+            <li className="navbar-item navbar-logout">
+                <Link
+                className="navbar-link"
+                to=""
+                onClick={() => {
+                    localStorage.removeItem("lounge_user")
+                    navigate("/", { replace: true })
+                }}
+                >
+                Logout
+                </Link>
+            </li>
+        ) : (
+          ""
+        )}
         </ul>
     )
 }
