f6b43f366f7fd9cd66d2aa5dbb12d2fe3648921c
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-18 10:39:00 -0500

    Added component to handle login

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// import "./Login.css"
+import { getReaderByEmail } from "../../service/readerService";
+
+export const Login = () => {
+  const [email, set] = useState("Enter Email");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    getReaderByEmail(email).then((foundReader) => {
+      if (foundReader.length === 1) {
+        const reader = foundReader[0];
+        localStorage.setItem(
+          "lounge_user",
+          JSON.stringify({
+            id: reader.id,
+          })
+        );
+
+        navigate("/");
+      } else {
+        window.alert("Invalid login");
+      }
+    });
+  };
+
+  return (
+    <main className="container-login">
+      <section>
+        <form className="form-login" onSubmit={handleLogin}>
+          <h1>Literary Lounge</h1>
+          <h2>Please sign in</h2>
+          <fieldset>
+            <div className="form-group">
+              <input
+                type="email"
+                value={email}
+                onChange={(evt) => set(evt.target.value)}
+                className="form-control"
+                placeholder="Email address"
+                required
+                autoFocus
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <div className="form-group">
+              <button className="login-btn btn-info" type="submit">
+                Sign in
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+      <section>
+        <Link to="/register">Not a member yet?</Link>
+      </section>
+    </main>
+  );
+};
