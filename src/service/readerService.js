96207f3bc3839fe5ef7c3ff864c77026dc8b8472
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-18 10:41:10 -0500

    Added service to handle creating a new user

--- src/service/readerService.js
+++ src/service/readerService.js
@@ -1,11 +1,33 @@
 export const getAllReaders = () => {
-    return fetch(`http://localhost:8088/readers?_embed=booksRead`).then(res => res.json())
-}
+  return fetch(`http://localhost:8088/readers?_embed=booksRead`).then((res) =>
+    res.json()
+  );
+};
 
 export const getReaderById = (readerId) => {
-    return fetch(`http://localhost:8088/readers/${readerId}?_embed=booksRead`).then(res => res.json())
-}
+  return fetch(
+    `http://localhost:8088/readers/${readerId}?_embed=booksRead`
+  ).then((res) => res.json());
+};
 
 export const getBooksByReaderId = (readerId) => {
-    return fetch(`http://localhost:8088/booksRead?readerId=${readerId}&_expand=book`).then(res => res.json())
-}
+  return fetch(
+    `http://localhost:8088/booksRead?readerId=${readerId}&_expand=book`
+  ).then((res) => res.json());
+};
+
+export const getReaderByEmail = (email) => {
+  return fetch(`http://localhost:8088/readers?email=${email}`).then((res) =>
+    res.json()
+  );
+};
+
+export const createReader = (reader) => {
+  return fetch("http://localhost:8088/readers", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reader),
+  }).then((res) => res.json());
+};
