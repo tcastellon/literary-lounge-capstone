66882950129430ebf22a01b3bcb939e5ca41d1df
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-19 10:39:52 -0500

    Added function to add book to booksRead

--- src/service/bookService.js
+++ src/service/bookService.js
@@ -1,13 +1,25 @@
 export const getAllBooks = () => {
-    return fetch(`http://localhost:8088/books`).then(res => res.json())
-}
+  return fetch(`http://localhost:8088/books`).then((res) => res.json());
+};
 
 export const getBooksById = (bookId) => {
-    return fetch(`http://localhost:8088/books/${bookId}`).then(res => res.json())
-}
+  return fetch(`http://localhost:8088/books/${bookId}`).then((res) =>
+    res.json()
+  );
+};
 
 export const getBooksByReaderId = (readerId) => {
   return fetch(
     `http://localhost:8088/booksRead?readerId=${readerId}&_expand=book`
   ).then((res) => res.json());
+};
+
+export const addBookToLibrary = (newReaderBook) => {
+  return fetch(`http://localhost:8088/booksRead`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newReaderBook),
+  }).then((res) => res.json());
 };
