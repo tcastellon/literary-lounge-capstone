8e6286bcf39af621a4955b194e9b36f184b10ae6
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-19 10:38:44 -0500

    Added functionality to button

--- src/components/books/BookDetails.jsx
+++ src/components/books/BookDetails.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getBooksById } from "../../service/bookService";
-
+import { addBookToLibrary, getBooksById, getBooksByReaderId } from "../../service/bookService";
 
 export const Book = ({ book }) => {
   return (
@@ -9,33 +8,53 @@ export const Book = ({ book }) => {
       <header>{book.title}</header>
       <img src={book.image} alt={book.title} />
     </section>
-  )
-}
+  );
+};
 
-export const BookDetails = () => {
-  const [book, setBook] = useState([])
-  const { bookId } = useParams()
+export const BookDetails = ({ currentReader }) => {
+  const [book, setBook] = useState([]);
+  const [readerBooks, setReaderBooks] = useState([]);
+  const { bookId } = useParams();
 
   useEffect(() => {
     getBooksById(bookId).then((bookObj) => {
-      setBook(bookObj)
+      setBook(bookObj);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getBooksByReaderId(currentReader.id).then((readerBooksArray) => {
+      setReaderBooks(readerBooksArray);
+    });
+  }, [currentReader]);
+
+  const handleAddToLibrary = (book) => {
+    const newReaderBook = {
+      readerId: currentReader.id,
+      bookId: book.id,
+      read: false
+    }
+    addBookToLibrary(newReaderBook).then((savedReaderBook) => {
+      setReaderBooks([...readerBooks, savedReaderBook])
     })
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
+  }
+
   return (
     <>
-    <div>
-      <header>{book.title}</header>
-      <div>{book.author}</div>
-      <div>{book.genre}</div>
-      <img src={book.image} alt={book.title}/>
-      <div>{book.description}</div>
-    </div>
-    <div>
-      <button>Add to Library</button>
-    </div>
+      <div>
+        <header>{book.title}</header>
+        <div>{book.author}</div>
+        <div>{book.genre}</div>
+        <img src={book.image} alt={book.title} />
+        <div>{book.description}</div>
+      </div>
+      <div>
+        {!readerBooks.some((readerBook) => readerBook.bookId === book.id) && (
+          <button onClick={() => handleAddToLibrary(book)}>Add to Library</button>
+        )}
+      </div>
     </>
   );
 };
