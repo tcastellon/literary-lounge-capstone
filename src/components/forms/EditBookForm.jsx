4755170421b31ba5b46e702606dd9fd7326d7c0e
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-22 20:02:19 -0500

    Added form to edit books

---
+++ src/components/forms/EditBookForm.jsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react";
+import { getBooksByReaderId, updateBook, updateBookRead } from "../../service/bookService";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const EditBookForm = ({ currentReader }) => {
+  const [book, setBook] = useState({});
+  const { bookId } = useParams();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getBooksByReaderId(currentReader.id).then((booksArray) => {
+        const bookObj = booksArray.find((book) => book.bookId === parseInt(bookId))
+      setBook(bookObj);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleUpdateBook = () => {
+    const updatedBook = {
+        id: book.book.id,
+        creatorId: book.book.creatorId,
+        title: book.book.title,
+        author: book.book.author,
+        genre: book.book.genre,
+        image: book.book.image,
+        description: book.book.description
+    }
+    const updatedBookRead = {
+        id: book.id,
+        readerId: book.readerId,
+        bookId: book.bookId,
+        read: book.read
+    }
+    updateBook(updatedBook)
+    updateBookRead(updatedBookRead)
+    navigate(`/readerlist/${currentReader.id}`)
+  }
+
+  return (
+    <form>
+      <h2>Edit Book</h2>
+      <fieldset>
+        <div>
+          <label>Book Title: </label>
+          <input
+            type="text"
+            placeholder={book.book?.title}
+            onChange={(event) => {
+              const bookCopy = { ...book };
+              bookCopy.book.title = event.target.value;
+              setBook(bookCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <label>Author: </label>
+          <input
+            type="text"
+            placeholder={book.book?.author}
+            onChange={(event) => {
+              const bookCopy = { ...book };
+              bookCopy.book.author = event.target.value;
+              setBook(bookCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <label>Genre: </label>
+          <input
+            type="text"
+            placeholder={book.book?.genre}
+            onChange={(event) => {
+              const bookCopy = { ...book };
+              bookCopy.book.genre = event.target.value;
+              setBook(bookCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <label>Image Link: </label>
+          <input
+            type="text"
+            placeholder={book.book?.image}
+            onChange={(event) => {
+              const bookCopy = { ...book };
+              bookCopy.book.image = event.target.value;
+              setBook(bookCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <label>Brief Description: </label>
+          <input
+            type="text"
+            placeholder={book.book?.description}
+            onChange={(event) => {
+              const bookCopy = { ...book };
+              bookCopy.book.description = event.target.value;
+              setBook(bookCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <label>
+            Read Book?
+            <input
+              type="checkbox"
+              onChange={(event) => {
+                const bookReadCopy = { ...book };
+                bookReadCopy.read = event.target.checked;
+                setBook(bookReadCopy);
+              }}
+            />
+          </label>
+        </div>
+      </fieldset>
+      <fieldset>
+        <div>
+          <button onClick={handleUpdateBook}>Save Book</button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
