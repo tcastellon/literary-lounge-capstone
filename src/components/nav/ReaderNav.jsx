28d624ce0701931f5e2e9d0af9dac61146d13cff
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-25 14:25:02 -0500

    Added link to add book form

--- src/components/nav/ReaderNav.jsx
+++ src/components/nav/ReaderNav.jsx
@@ -1,37 +1,40 @@
-import { Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
-import "./Nav.css"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
 
 export const ReaderNav = () => {
-    const navigate = useNavigate()
-    
-    return (
-        <ul className="navbar">
-            <li className="navbar-item">
-                <Link to="/">Home</Link>
-            </li>
-            <li className="navbar-item">
-                <Link to="/booklist">Books</Link>
-            </li>
-            <li className="navbar-item">
-                <Link to="/readerlist">Readers</Link>
-            </li>
-            {localStorage.getItem("lounge_user") ? (
-            <li className="navbar-item navbar-logout">
-                <Link
-                className="navbar-link"
-                to=""
-                onClick={() => {
-                    localStorage.removeItem("lounge_user")
-                    navigate("/", { replace: true })
-                }}
-                >
-                Logout
-                </Link>
-            </li>
-        ) : (
-          ""
-        )}
-        </ul>
-    )
-}
+  const navigate = useNavigate();
+
+  return (
+    <ul className="navbar">
+      <li className="navbar-item">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/booklist">Books</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/readerlist">Readers</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/addbook">Add Book</Link>
+      </li>
+      {localStorage.getItem("lounge_user") ? (
+        <li className="navbar-item navbar-logout">
+          <Link
+            className="navbar-link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("lounge_user");
+              navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
